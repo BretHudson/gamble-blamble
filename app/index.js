02d0503c113c392d4ab4532347f23cf5b48b987b
Bret Hudson <bret@brethudson.com>
2021-10-25 13:10:18 -0700

    Set up Express routing

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-console.log('GAMBLE BLAMBLE');
+const express = require('express');
+
+const ENV_DEV = 'development';
+const ENV_PROD = 'production';
+
+const {
+	ENV = ENV_PROD,
+	PORT = 3000,
+} = process.env;
+
+const app = express();
+
+app.route('/bet')
+	.get((req, res) => {
+		res.send('Here are all the bets!');
+	})
+	.post((req, res) => {
+		res.send('Added bet');
+	})
+	.delete((req, res) => {
+		res.send('Deleted bet');
+	});
+
+app.listen(PORT, () => {
+	console.log(`GAMBLE BLAMBLE listening on port ${PORT}`);
+});
+
