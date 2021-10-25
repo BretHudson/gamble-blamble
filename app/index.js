bee9280b356328882e9f95b6e391b0a95f31ecd3
Bret Hudson <bret@brethudson.com>
2021-10-25 13:44:44 -0700

    Added and set up pg (node-postgres) npm package

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const ENV_DEV = 'development';
 const ENV_PROD = 'production';
@@ -8,20 +9,28 @@ const {
 	PORT = 3000,
 } = process.env;
 
-const app = express();
+(async () => {
+	const pgClient = new Client();
+	await pgClient.connect();
+	
+	console.log('Connected to postgres database');
+	
+	const app = express();
 
-app.route('/bet')
-	.get((req, res) => {
-		res.send('Here are all the bets!');
-	})
-	.post((req, res) => {
-		res.send('Added bet');
-	})
-	.delete((req, res) => {
-		res.send('Deleted bet');
+	app.route('/bet')
+		.get((req, res) => {
+			res.send('Here are all the bets!');
+		})
+		.post((req, res) => {
+			res.send('Added bet');
+		})
+		.delete((req, res) => {
+			res.send('Deleted bet');
+		});
+
+	app.listen(PORT, () => {
+		console.log(`GAMBLE BLAMBLE listening on port ${PORT}`);
 	});
+})();
 
-app.listen(PORT, () => {
-	console.log(`GAMBLE BLAMBLE listening on port ${PORT}`);
-});
 
