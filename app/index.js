51bfbd7bdf0e6a5c01459f767e1b72917329cf0c
Bret Hudson <bret@brethudson.com>
2021-10-25 14:10:10 -0700

    Initializing databases on app startup

--- app/index.js
+++ app/index.js
@@ -9,11 +9,69 @@ const {
 	PORT = 3000,
 } = process.env;
 
+const initDatabase = async pgClient => {
+	const teamsTableName = 'teams';
+	const betsTableName = 'bets';
+	
+	await pgClient.query(
+		`CREATE TABLE IF NOT EXISTS ${teamsTableName} (
+			id		SERIAL PRIMARY KEY,
+			name	VARCHAR(32) UNIQUE NOT NULL
+		)`
+	);
+	
+	const result = await pgClient.query(`SELECT * FROM ${teamsTableName}`);
+	if (result.rows.length === 0) {
+		await Promise.all([
+			'Hellmouth Sunbeams',
+			'Charleston Shoe Thieves',
+			'Yellowstone Magic',
+			'LA Unlimited Tacos',
+			'Houston Spies',
+			'Miami Dale',
+			'Breckenridge Jazz Hands',
+			'Kansas City Breath Mints',
+			'Hawai\'i Fridays',
+			'Chicago Firefighters',
+			'Canada Moist Talkers',
+			'Ohio Worms',
+			'Mexico City Wild Wings',
+			'Tokyo Lift',
+			'New York Millennials',
+			'Philly Pies',
+			'Baltimore Crabs',
+			'Hades Tigers',
+			'Atlantis Georgias',
+			'Seattle Garages',
+			'Dallas Steaks',
+			'Core Mechanics',
+			'Boston Flowers',
+			'San Francisco Lovers',
+		].map(async teamName => {
+			const insertQuery = `INSERT INTO ${teamsTableName}(name) VALUES($1)`;
+			await pgClient.query(insertQuery, [teamName]);
+		}));
+	}
+	
+	await pgClient.query(
+		`CREATE TABLE IF NOT EXISTS ${betsTableName} (
+			id		SERIAL PRIMARY KEY,
+			team_id	INT NOT NULL,
+			amount	INT NOT NULL,
+			FOREIGN KEY (team_id)
+				REFERENCES teams (id)
+		)`
+	);
+};
+
 (async () => {
 	const pgClient = new Client();
 	await pgClient.connect();
 	
 	console.log('Connected to postgres database');
+	
+	// Initialize database if null
+	await initDatabase(pgClient);
 	
 	const app = express();
 
